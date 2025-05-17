df694f3764fa557f6ea05e48412e4c5ee1271d59
bamsgrtt <bambang031106@gmail.com>
2025-05-17 11:32:44 +0700

    add commonclass

--- Belajar-java-dasar-main/Belajar-Java-Dasar/src/Block.java
+++
@@ -1,8 +0,0 @@
-public class Block {
-    public static void main(String[] args) { // ini block
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-    }
-}
--- Belajar-java-dasar-main/Belajar-Java-Dasar/src/Percabangan/TernaryOperator.java
+++
@@ -1,9 +0,0 @@
-package Percabangan;
-
-public class TernaryOperator {
-    public static void main(String[] args) {
-        var nilai = 70;
-        String ucapan = nilai >= 75 ? "Anda lulus" : "Anda tidak lulus";
-        System.out.println(ucapan);
-    }
-}
